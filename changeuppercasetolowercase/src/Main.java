fe2e73884a865acd1e96431d101654395efa5dbe
DeepSeeDev <hyuk2000s@naver.com>
2025-03-27 12:23:10 +0900

    2. 대소문자 변환

--- changeuppercasetolowercase/src/Main.java
+++ changeuppercasetolowercase/src/Main.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+
+        String answer = "";
+//        for( char c : str.toCharArray()){
+//            if(Character.isLowerCase(c)){
+//                answer += Character.toUpperCase(c);
+//            }
+//            else{
+//                answer += Character.toLowerCase(c);
+//            }
+//        }
+
+        // 대문자일경우
+        for(char c : str.toCharArray()){
+            if( c >= 65 && c <= 90){
+                answer += (char)(c + 32);
+            }
+            else {
+                answer += (char)(c - 32);
+            }
+        }
+
+        System.out.println(answer);
     }
 }
