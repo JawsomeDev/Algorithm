21cf40fd3fd78d4e9ab5357e515c64d71beeb958
DeepSeeDev <hyuk2000s@naver.com>
2025-04-07 10:25:32 +0900

    5. 소수(에라토스테네스 체)

---
+++ chpater02-5/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n+1];
+        int answer= 0;
+        for(int i = 2; i < n; i++){
+            if(arr[i] == 0){
+                answer++;
+                for(int j = i; j<n; j= j+i)
+                    arr[j] = 1;
+            }
+        }
+        System.out.println(answer);
+    }
+}
