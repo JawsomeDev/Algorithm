5d9af5cfc02053f9a907ca66eeb78a4731b1c1b4
DeepSeeDev <hyuk2000s@naver.com>
2025-04-30 12:34:35 +0900

    6. 공주 구하기

---
+++ chapter05-6/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        int k = input.nextInt();
+        Queue<Integer> Q = new LinkedList<>();
+        int answer = 0;
+        for (int i = 1; i <= n; i++) Q.offer(i);
+        while (!Q.isEmpty()) {
+            for (int i = 1; i < k; i++) Q.offer(Q.poll());
+            Q.poll();
+            if(Q.size() == 1) answer = Q.poll();
+        }
+        System.out.println(answer);
+    }
+}
