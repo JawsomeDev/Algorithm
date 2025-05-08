f4a25f8e77f88845369f4ac4fbec7ddd3ec9e10e
DeepSeeDev <hyuk2000s@naver.com>
2025-05-08 20:04:12 +0900

    3. 삽입 정렬

--- chapter06-2/src/Main.java
+++ chapter06-2/src/Main.java
@@ -8,12 +8,12 @@ public class Main {
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
-        for(int i = 0; i < n; i++){
-            for(int j = i+1; j < n; j++){
-                if(arr[i] > arr[j]){
-                    int temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
+        for(int i = 0; i < n-1; i++){
+            for(int j = 0; j < n-i-1; j++){
+                if(arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
                 }
             }
         }
